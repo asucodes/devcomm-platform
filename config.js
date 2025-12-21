ab91f26749bd8e2d64aa0532f0a4ec2d61777631
root <root@Asu.localdomain>
2025-12-21 16:10:58 +0000

    Initial commit: Basic project structure setup

---
+++ config.js
@@ -0,0 +1,11 @@
+// DevComm Platform Configuration
+// Version: 0.1.0-alpha
+
+const config = {
+    appName: "DevComm Stream",
+    port: 3000,
+    theme: "dark",
+    maintenanceMode: false
+};
+
+module.exports = config;
